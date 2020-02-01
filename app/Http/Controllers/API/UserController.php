0f2268cc122d5bdcbb0d5ee63087583ddf5149d8
Alejandro Sánchez Quesada <48472291+7963289@users.noreply.github.com>
2020-02-01 20:34:53 +0100

    Comentada una linea que no tenia que estar ahi

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
     public function index()
     {
 
-        return MateriaResource::collection(Auth::user()->misMateriasMatriculadas);
+        /* return MateriaResource::collection(Auth::user()->misMateriasMatriculadas); */
     }
 
     /**
